16a061658ec4a8188a5a99f95ec0b1b2be2e61de
Nicolas Jensen <nicolasJensen9@gmail.com>
2020-01-22 14:30:34 +1000

    initial setup for the client

--- src/App.js
+++ src/App.js
@@ -1,8 +1,34 @@
 import React from 'react'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  useRouteMatch,
+  useParams
+} from "react-router-dom"
+
+import Header from './components/Header'
+import Footer from './components/Footer'
 
 function App() {
   return (
-    <div className="App"></div>
+    <div className="App">
+      <Router>
+        <Header />
+
+        <Switch>
+          <Route path="/">
+          </Route>
+          <Route path="/">
+          </Route>
+          <Route path="/">
+          </Route>
+        </Switch>
+
+        <Footer />
+      </Router>
+    </div>
   )
 }
 
---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Link } from "react-router-dom"
+
+function Header() {
+  return (
+    <div className="Header">
+      <Link to='/'>Home</Link>
+      <Link to='/'>Home</Link>
+      <Link to='/'>Home</Link>
+      <Link to='/'>Home</Link>
+    </div>
+  )
+}
+
+export default Header
